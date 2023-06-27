def1e9f776b60451dc75b5542b8b3d348e6df081
Myriam Rajeriarisoa <myrajeriarisoa@yahoo.fr>
2023-06-27 15:59:01 +0200

    JS - exercice DEFI - Les fonctions : premier commit

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //Exercice : Cours Initiation Javascript - Les fonctions 
-//Mettre le début d'un mot en majuscule et le reste en minuscule
+//DEFI : calcul de prix lors de solde, d'une boutique en ligne
 
-let WORD = prompt("Taper un mot : ")
+/* Prix des produits sans réduction */
+let chaise = 120.50
+let table = 1200.99
+let vase = 420.01
+let canape = 12585.55
 
-function wordCase(word) {
-    let fistLetterWord = word.substring(0,1)
-    let otherLetterWord = word.substring(1)
-    console.log("Votre mot :  " + fistLetterWord.toUpperCase() + otherLetterWord.toLowerCase());
+// let nom = "ta voiture est jaune"
+// const regex2 = /jaune/i;
+/* Fonction qui calcul le prix soldé : en paramètres le produit et le taux de réduction */
+function reducPrice(chaise, rate) {
+let calcPrice 
+calcPrice = chaise * (1-rate)
+//console.log (` Le prix avec réduction est ${newCalcPrice.replace(regex, ',')} € `)
+return calcPrice
 }
+// let priceProduct = prompt ("Donner le prix avant reduction, de la chaise : ")
+// reducPrice(priceProduct)
+//let product = prompt ("Quel est le produit : ")
 
-wordCase()
+//let newPrice = reducPrice(chaise, 0.25)
+let euroPrice = reducPrice(chaise, 0.25) + "€"
+let newEuroPrice = euroPrice.replace(".", ",")
+
+console.log(`Le prix de la chaise avec réduction est ${newEuroPrice} `)
+
+
