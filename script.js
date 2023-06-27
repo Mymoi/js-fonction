018caa0e8c7d6070e5e04f93e4a5b316f9b56d58
Myriam Rajeriarisoa <myrajeriarisoa@yahoo.fr>
2023-06-27 19:08:49 +0200

    Cours JS - Les fonctions - DEFI - Boucle avec listes de produits et prix

--- script.js
+++ script.js
@@ -7,23 +7,63 @@ let table = 1200.99
 let vase = 420.01
 let canape = 12585.55
 
-// let nom = "ta voiture est jaune"
-// const regex2 = /jaune/i;
+let rate = 0.25
+let product = chaise
+
 /* Fonction qui calcul le prix soldé : en paramètres le produit et le taux de réduction */
-function reducPrice(chaise, rate) {
-let calcPrice 
-calcPrice = chaise * (1-rate)
-//console.log (` Le prix avec réduction est ${newCalcPrice.replace(regex, ',')} € `)
-return calcPrice
+function reducPrice(product, rate) {
+  let calcPrice 
+  calcPrice = product * (1-rate)
+  return calcPrice
 }
+
+let chaisePrice = reducPrice(chaise, 0.25)
+// console.log(`Le prix de la chaise :
+// sans réduction est ${chaise}€
+// avec réduction est ${chaisePrice}€ `)
 // let priceProduct = prompt ("Donner le prix avant reduction, de la chaise : ")
-// reducPrice(priceProduct)
-//let product = prompt ("Quel est le produit : ")
+// let reducPorcentage = prompt ("Donner le pourcentage de réduction : ")
+//  function rate (n) {
+//   if (Number.isInteger(n)) {
+//     return n / 100
+//   }
+//   else if (Number.isNaN(n)) {
+//     console.log ("Ce n'est pas un nombre")
+//   }
+//   else return
+// }
 
-//let newPrice = reducPrice(chaise, 0.25)
-let euroPrice = reducPrice(chaise, 0.25) + "€"
-let newEuroPrice = euroPrice.replace(".", ",")
-
-console.log(`Le prix de la chaise avec réduction est ${newEuroPrice} `)
 
 
+// Prix avec les précisions : , et €
+function euroPrice (chaisePrice) {
+  let euroChaisePrice = chaisePrice + " "
+  let newChaisePrice = euroChaisePrice.replace (".", ",")
+  return newChaisePrice
+}
+//console.log(`Chaise prix sans réduction : ${chaise} € `)
+//avec réduction est ${euroPrice (chaisePrice)}€ `)
+
+
+//Prix arrondi à l'entier supérieur
+function roundSalePrice (chaisePrice) {
+  let roundChaisePrice = Math.ceil(chaisePrice)
+  return roundChaisePrice
+}
+//console.log(`Chaise soldé à prix rond : ${roundSalePrice (chaisePrice)} € `)
+
+// Calcul du prix soldé pour chaque produit : chaise table vase canapé
+const listProduc = ["chaise","table","vase","canape"]
+const prices = [120.50,1200.99,420.01,12585.55]
+let initPrice
+let item
+let priceSale
+let reducRate = 0.25
+console.log(`Voici les produits soldés à ${reducRate * 100} % :`) 
+for (let i = 0; i < listProduc.length; i++) {
+  item = listProduc[i]
+  initPrice = prices[i] * 1
+  priceSale = reducPrice(initPrice, reducRate)
+  roundPrice = roundSalePrice (priceSale)
+  console.log(`${item} : ${roundPrice} € ` )
+}
